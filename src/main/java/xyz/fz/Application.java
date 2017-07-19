5309e534845204f6d212072f28253e3a3fc36767
Fz <natureorz@foxmail.com>
2017-07-20 00:29:35 +0800

    spring cloud eureka server

---
+++ src/main/java/xyz/fz/Application.java
@@ -0,0 +1,17 @@
+package xyz.fz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
